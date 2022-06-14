c7fd2500997397f50882f705089943c6f96f5301
Udai Gupta <udaigupta19311@gmail.com>
2022-06-14 11:48:42 +0530

    Added menubar with custom row components

---
+++ app/javascript/src/components/Settings/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { Route, Switch } from "react-router-dom";
+
+import MenubarComponent from "./MenubarComponent";
+
+function Settings() {
+  return (
+    <div className="flex">
+      <MenubarComponent />
+      <Switch>
+        <Route
+          exact
+          path="/settings/redirections"
+          component={() => <div>redirections Page</div>}
+        />
+        <Route exact path="/settings/categories">
+          <div>categories Page</div>
+        </Route>
+        <Route
+          exact
+          path="/settings"
+          component={() => <div>Seeting Page</div>}
+        />
+      </Switch>
+    </div>
+  );
+}
+
+export default Settings;
